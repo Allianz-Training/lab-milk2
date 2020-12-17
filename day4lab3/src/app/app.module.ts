ff28ff3a627bcbc593f2518ecef1c90fa3d5805a
Ungsumalin <74392251+Ungsumalin@users.noreply.github.com>
2020-12-17 17:17:19 +0700

    commit3

---
+++ day4lab3/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PostListComponent } from './post-list/post-list.component';
+
+@NgModule({
+ declarations: [AppComponent, PostListComponent],
+ imports: [BrowserModule, HttpClientModule],
+ providers: [],
+ bootstrap: [AppComponent]
+})
+export class AppModule {}
