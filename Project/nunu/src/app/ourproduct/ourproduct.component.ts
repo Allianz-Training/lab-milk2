e9ed5b7113ba370f1c8cf78df00142490fdc2329
keattisakTrpp <keattisak.trpp@gmail.com>
2021-01-01 13:38:47 +0700

    show profile info

--- Project/nunu/src/app/ourproduct/ourproduct.component.ts
+++ Project/nunu/src/app/ourproduct/ourproduct.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-ourproduct',
@@ -10,7 +12,7 @@ export class OurproductComponent implements OnInit {
 
   products: any
 
-  constructor(private api : ApiService) {
+  constructor(private api : ApiService, private userService: UserService, private router: Router) {
     this.products = [];
   }
 
@@ -21,7 +23,12 @@ export class OurproductComponent implements OnInit {
   }
 
   buy(productId: string): void {
-    console.log(this.products.filter(p => p.id === productId)[0])
+    // console.log(this.products.filter(p => p.id === productId)[0])
+    if(this.userService.getToken()) {
+      this.router.navigate(['/cart'])
+    } else {
+      this.router.navigate(['/login'])
+    }
   }
 
 }
