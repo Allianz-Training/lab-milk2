78c8da6f5896ca2d6b9566424638901e03dc22ec
jinnykaew <nunu_pukpui@hotmail.com>
2021-01-02 01:00:30 +0700

    payrate and history claim spec

--- Project/nunu/src/app/payrate/payrate.component.spec.ts
+++ Project/nunu/src/app/payrate/payrate.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PayrateComponent } from './payrate.component';
 
@@ -8,6 +9,7 @@ describe('PayrateComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports : [FormsModule,ReactiveFormsModule],
       declarations: [ PayrateComponent ]
     })
     .compileComponents();
