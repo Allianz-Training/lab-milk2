9e6623356cc3d8342c6f11ff63bd751e4ca8fbfe
jinnykaew <nunu_pukpui@hotmail.com>
2021-01-02 01:16:23 +0700

    edit register

--- Project/nunu/src/app/register/register.component.spec.ts
+++ Project/nunu/src/app/register/register.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RegisterComponent } from './register.component';
 
@@ -9,7 +10,7 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports : [ FormsModule,ReactiveFormsModule],
+      imports : [ FormsModule,ReactiveFormsModule,RouterTestingModule],
       declarations: [ RegisterComponent ]
     })
     .compileComponents();
