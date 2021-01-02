816327080e0c258439196ae0b652ff756fd68629
keattisakTrpp <keattisak.trpp@gmail.com>
2021-01-02 12:59:23 +0700

    prepare register for api

--- Project/nunu/src/app/register/register.component.ts
+++ Project/nunu/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { error } from 'protractor';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-register',
@@ -11,13 +13,14 @@ export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router, private api: ApiService) {
     this.registerForm = fb.group({
       email: ["", [Validators.required, Validators.email]],
       password: ["", [Validators.required, Validators.minLength(8)]],
       firstName: ["", Validators.required],
       lastName: ["",Validators.required],
       phone: ["", [Validators.required, Validators.pattern(/^0\d{9}$/), Validators.maxLength(10)]],
+      address: [""],
       petName: ["",Validators.required],
       petAge: ["", Validators.required],
       breed: ["", Validators.required],
@@ -34,7 +37,27 @@ export class RegisterComponent implements OnInit {
     if(this.registerForm.invalid) {
       alert("Please fill form")
     } else {
-      this.router.navigate(['/login']);
+      const { firstName, lastName, email, password, phone, address,
+        petName, petAge, breed, gender
+      } = this.registerForm.value;
+      const body = {
+        user: {
+          firstname: firstName,
+          lastname: lastName,
+          email,
+          phone,
+          address,
+          citizenId: this.registerForm.get('IDcard'),
+          password
+        },
+        pet: {
+          name: petName,
+          age: petAge,
+          breed, gender
+        }
+      }
+      console.log(body)
+      // this.api.post('users/signup', body).subscribe(res => this.router.navigate(['/login']), error => alert(error))
     }
   }
 
